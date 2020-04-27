3db138b74f0042478dd64c182cc37df068dd6cbe
toufupig <1406380135@qq.com>
2020-04-27 22:46:43 +0800

    添加啦啦啦

--- src/com/toufupig/first/CommitDemo.java
+++ src/com/toufupig/first/CommitDemo.java
@@ -5,6 +5,7 @@ public class CommitDemo {
     public static void main(String[] args) {
         System.out.println("这是我的第一个Git项目");
         System.out.println("hahah");
+        System.out.println("添加啦啦啦");
     }
 
 }
